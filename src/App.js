eae2ccf982253e8de62a0c732260aef5dfccded4
ryanthecorsair <58884290+ryanthecorsair@users.noreply.github.com>
2021-02-21 07:29:14 -0500

    Including modified files.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png';
 
 class App extends Component {
     state = { displayBio: false };
@@ -7,9 +10,10 @@ class App extends Component {
         this.setState({ displayBio: !this.state.displayBio });
     }
 
-    render = () => {
+    render() {
         return (
             <div>
+                <img src={profile} alt='profile' className="profile" />
                 <h1>Hello!</h1>
                 <p>My name is Ryan. I'm a software engineer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
@@ -27,6 +31,10 @@ class App extends Component {
                         </div>
                     )
                 }
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
             </div>
         )
     }
